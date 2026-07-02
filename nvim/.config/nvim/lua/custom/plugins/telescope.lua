87ee6c2b14948f8404b1e3a1d6c81832f625f574
Brandon Burkett <brandonburkett01@gmail.com>
2026-07-02 00:30:50 -0400

    feat: update nvim plugins and telescope hidden file search

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -46,20 +46,26 @@ return {
 		-- [[ Configure Telescope ]]
 		-- See `:help telescope` and `:help telescope.setup()`
 		require("telescope").setup({
-			-- You can put your default mappings / updates / etc. in here
-			--  All the info you're looking for is in `:help telescope.setup()`
-			--
-			-- defaults = {
-			--   mappings = {
-			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-			--   },
-			-- },
-			-- pickers = {}
 			defaults = {
-				file_ignore_patterns = {
-					"node_modules",
+				file_ignore_patterns = { "node_modules" },
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+					"--glob",
+					"!.git/*",
 				},
 			},
+			pickers = {
+				find_files = { hidden = true },
+			},
+			--  All the info you're looking for is in `:help telescope.setup()`
+			--
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown(),
